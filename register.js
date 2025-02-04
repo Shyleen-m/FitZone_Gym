5f80f0be5b602220ed6acddfcfc1b55500441921
Shyleen-m <ngaakudzweishem@gmail.com>
2025-02-04 01:58:05 +0100

    Update register.js

--- register.js
+++ register.js
@@ -1,17 +1,11 @@
 function handleSubmit(event) {
-    event.preventDefault(); // Prevent the default form submission
+    event.preventDefault(); 
 
-    // Get form values
     const name = document.getElementById("name").value;
     const age = document.getElementById("age").value;
     const height = document.getElementById("height").value;
     const weight = document.getElementById("weight").value;
     const email = document.getElementById("email").value;
-
-    // Display a success message or handle the data as needed
+    
     alert(`Registration Successful!`);
-
-    // Optionally, you can redirect to another page or clear the form
-     window.location.href = "index.html"; // Uncomment to redirect
-    // document.getElementById("registrationForm").reset(); // Uncomment to reset the form
-}
+window.location.href = "index.html";} 
