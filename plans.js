6af89d8c9e9576e458cd7ba86291d3d95f1a47c9
Shyleen-m <ngaakudzweishem@gmail.com>
2025-02-04 01:56:53 +0100

    Update plans.js

--- plans.js
+++ plans.js
@@ -2,12 +2,6 @@ function showPlanInfo() {
     const selectedPlan = document.getElementById("planSelect").value;
     const allPlans = document.querySelectorAll(".plan-info");
 
-    // Hide all plans first
-    ////allPlans.forEach(plan => {
-     //   plan.style.display = "none";
-    //});
-
-    // Display the selected plan
     if (selectedPlan) {
         document.getElementById(selectedPlan).style.display = "block";
     }
